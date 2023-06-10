cac99e560760b7fd3712241f093a31d6a38c271c
imanwarsame <iwarsame38@gmail.com>
2023-06-10 21:10:25 +0100

    Added gallery

--- warsamestudio/app/page.tsx
+++ warsamestudio/app/page.tsx
@@ -1,100 +1,136 @@
 'use client';
 import { Grid, ImageList, ImageListItem, Typography } from '@mui/material';
+import { motion } from 'framer-motion';
+import React from 'react';
+import './styles.css';
 
 export default function Home() {
+	const [galleryVisible, setGalleryVisible] = React.useState(false);
 	const itemData = [
 		{
-			img: './13MercerStreet-3.1.jpg',
+			src: './13MercerStreet-3.1.jpg',
 			title: 'Bed',
 		},
 		{
-			img: './4SlingsbyPlace-2.jpg',
+			src: './4SlingsbyPlace-2.jpg',
 			title: 'Books',
 		},
 		{
-			img: './7MercerStreet-4.jpg',
+			src: './7MercerStreet-4.jpg',
 			title: 'Sink',
 		},
 		{
-			img: './DSC_9028.jpg',
+			src: './DSC_9028.jpg',
 			title: 'Kitchen',
 		},
 		{
-			img: './DSC_9080.jpg',
+			src: './DSC_9080.jpg',
 			title: 'Kitchen',
 		},
 		{
-			img: './DSC_9096.jpg',
+			src: './DSC_9096.jpg',
 			title: 'Kitchen',
 		},
 		{
-			img: './DSC_9118.jpg',
+			src: './DSC_9118.jpg',
 			title: 'Kitchen',
 		},
 		{
-			img: './DSC_9123.jpg',
+			src: './DSC_9123.jpg',
 			title: 'Kitchen',
 		},
 		{
-			img: './DSC_9180.jpg',
+			src: './DSC_9180.jpg',
 			title: 'Kitchen',
 		},
 		{
-			img: './DSC_9252.jpg',
+			src: './DSC_9252.jpg',
 			title: 'Kitchen',
 		},
 		{
-			img: './DSC_9312.jpg',
+			src: './DSC_9312.jpg',
 			title: 'Kitchen',
 		},
 		{
-			img: './TheShard.jpg',
+			src: './TheShard.jpg',
 			title: 'Kitchen',
 		},
 		{
-			img: './DSC_9377.jpg',
+			src: './DSC_9377.jpg',
 			title: 'Kitchen',
 		},
 		{
-			img: './DSC_9384.jpg',
+			src: './DSC_9384.jpg',
 			title: 'Kitchen',
 		},
 		{
-			img: './DSC_9468.jpg',
+			src: './DSC_9468.jpg',
 			title: 'Kitchen',
 		},
 		{
-			img: './DSC_9472.jpg',
+			src: './DSC_9472.jpg',
 			title: 'Kitchen',
 		},
 		{
-			img: './DSC_9477.jpg',
+			src: './DSC_9477.jpg',
 			title: 'Kitchen',
 		},
 		{
-			img: './DSC_9498.jpg',
+			src: './DSC_9498.jpg',
 			title: 'Kitchen',
 		},
 		{
-			img: './DSC_9125.jpg',
+			src: './DSC_9125.jpg',
 			title: 'Kitchen',
 		},
-	  ];
+	];
+
+	React.useEffect(() => {
+		itemData.forEach((picture) => {
+			new Image().src = picture.src;
+		});
+
+		setGalleryVisible(true);
+	})
+
+	const container = {
+		hidden: { opacity: 1, scale: 0 },
+		visible: {
+			opacity: 1,
+			scale: 1,
+			transition: {
+			delayChildren: 0.5,
+			staggerChildren: 0.2
+			}
+		}
+	};
+	  
+	const item = {
+		hidden: { y: 20, opacity: 0 },
+		visible: {
+			y: 0,
+			opacity: 1
+		}
+	};
 
 	return (
 		<Grid container spacing={0} sx={{ width: '75%', mb: 5, mt: 1 }}>
 			<Grid item xs={12} sx={{ backgroundColor: 'transparent' }}>
+				{/* <PhotoAlbum layout='masonry' photos={itemData}/> */}
+				{galleryVisible &&
 				<ImageList variant="masonry" cols={2} gap={8}>
-					{itemData.map((item) => (
-						<ImageListItem key={item.img}>
-							<img
-							src={item.img}
-							srcSet={item.img}
-							alt={item.title}
-							/>
-						</ImageListItem>
+					<motion.ul className="container" variants={container} initial="hidden" animate="visible">
+					{itemData.map((index) => (
+						<motion.li key={index.src} className="item" variants={item}>
+							<ImageListItem key={index.src}>
+								<img src={index.src} srcSet={index.src} alt={index.title} loading='lazy'/>
+							</ImageListItem>
+						</motion.li>
 					))}
+					</motion.ul>
 				</ImageList>
+				}
+				<Typography>Hello</Typography>
 			</Grid>
 		</Grid>
 	);
