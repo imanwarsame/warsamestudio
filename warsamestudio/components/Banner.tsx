03b4b0498d206bbeb8013e0ebe10fef9973ac49b
imanwarsame <iwarsame38@gmail.com>
2023-06-17 17:35:31 +0100

    Added background image

--- warsamestudio/components/Banner.tsx
+++ warsamestudio/components/Banner.tsx
@@ -1,21 +1,31 @@
 'use client';
 import * as React from 'react';
-import { useTheme } from '@mui/material/styles';
-import { AppBar, Box, Toolbar, Button, Typography, Stack } from '@mui/material';
 import Lottie from 'lottie-react';
-import scrollAnimation from '../public/scroll-down.json';
+import { AppBar, Box, Toolbar, Button, Typography, Stack, useMediaQuery } from '@mui/material';
+import animatedScrollIcon from '../public/scroll-down.json';
+import Hero from '../public/Hero.jpg';
 
 export default function Banner() {
-	const theme = useTheme();
+	const imageStyles = {
+		height: '100vh',
+		backgroundImage: `url(${Hero.src})`,
+		backgroundRepeat: 'no-repeat',
+		backgroundPosition: 'center center',
+		backgroundSize: 'cover',
+		backgroundAttachment: 'fixed'
+	};
 
+	//Boolean that checks whether the screen is larger than 1000 px to define text size
+	const matches = useMediaQuery('(min-width:1300px)');
+	
 	return (
-		<Box display='flex' alignItems='center' justifyContent='center' sx={{ height: '100vh', backgroundColor: theme.palette.primary.main }}>
+		<Box display='flex' alignItems='center' justifyContent='center' sx={imageStyles}>
 			<Stack direction='column' display='flex' alignItems='center' spacing={4}>
-				<Typography variant='h1' align='center' color='white'>Architectural Photography</Typography>
+				<Typography variant={matches ? 'h1' : 'h3'} align='center' color='white'>Architectural Photography</Typography>
 				<Typography variant='body2' align='center' color='white'>London</Typography>
 			</Stack>
 			<Box display='flex' alignItems='center' justifyContent='center' sx={{ position: 'absolute', bottom: '20px' }}>
-				<Lottie animationData={scrollAnimation} height={50} width={50} />
+				<Lottie animationData={animatedScrollIcon} height={50} width={50} />
 			</Box>
 		</Box>
 	);
