ebc8258133714b5d2316d4be78b0ccd2b1237fe7
imanwarsame <iwarsame38@gmail.com>
2023-06-17 19:52:52 +0100

    Navbar colours are changing correctly

--- warsamestudio/components/Navbar.tsx
+++ warsamestudio/components/Navbar.tsx
@@ -6,6 +6,7 @@ import { useTheme } from '@mui/material/styles';
 
 export default function Navbar() {
 	const [isScrollPastBanner, setIsScrollPastBanner] = React.useState(false);
+	const [isDefaultPath, setDefaultPath] = React.useState(window.location.pathname === '/');
 	const theme = useTheme();
 
 	/* React hook that sets the state of `isScrollPastBanner` to `true` if the user has scrolled past the height
@@ -29,30 +30,57 @@ export default function Navbar() {
 	  };
 	}, []);
 
+	//Select colour based on page and scroll position
+	const linkColour = isScrollPastBanner || !isDefaultPath ? theme.palette.secondary.main : 'white';	
+
 	return (
 		<Box sx={{ flexGrow: 1 }}>
-			<AppBar position='fixed' color='transparent' sx={{ backdropFilter:"blur(5px)", borderRadius: '0px' }}>
-				<Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
-				<Link type='button' href="/" 
-					sx={{ '&:hover': { backgroundColor: 'transparent' }, 
-					color: isScrollPastBanner ? theme.palette.secondary.main : 'white', ml: 2, textTransform: 'capitalize', typography: 'h6' }}
+		<AppBar position='fixed' color='transparent' sx={{ backdropFilter: "blur(5px)", borderRadius: '0px' }}>
+			<Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+			<Link
+				type='button'
+				href="/"
+				onClick={() => setDefaultPath(true)}
+				sx={{
+				'&:hover': { backgroundColor: 'transparent' },
+				color: linkColour,
+				ml: 2,
+				textTransform: 'capitalize',
+				typography: 'h6'
+				}}
+			>
+				Warsame Studio
+			</Link>
+			<Box>
+				<Link
+				type='button'
+				href="/about"
+				onClick={() => setDefaultPath(false)}
+				sx={{
+					color: linkColour,
+					ml: 2,
+					textTransform: 'capitalize',
+					typography: 'body2'
+				}}
 				>
-					Warsame Studio
+				About
 				</Link>
-				<Box>
-					<Link type='button' href="/about" 
-					sx={{ color: isScrollPastBanner ? theme.palette.secondary.main : 'white', ml: 2, textTransform: 'capitalize', typography: 'body2' }}
-					>
-					About
-					</Link>
-					<Link type='button' href="/contact" 
-					sx={{ color: isScrollPastBanner ? theme.palette.secondary.main : 'white', ml: 2, textTransform: 'capitalize', typography: 'body2' }}
-					>
-					Contact
-					</Link>
-				</Box>
-				</Toolbar>
-			</AppBar>
+				<Link
+				type='button'
+				href="/contact"
+				onClick={() => setDefaultPath(false)}
+				sx={{
+					color: linkColour,
+					ml: 2,
+					textTransform: 'capitalize',
+					typography: 'body2'
+				}}
+				>
+				Contact
+				</Link>
+			</Box>
+			</Toolbar>
+		</AppBar>
 		</Box>
 	);
 }
