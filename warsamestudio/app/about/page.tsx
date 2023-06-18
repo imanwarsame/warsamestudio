bc3eecc002fc6b666dabf4565677c89ae72ba50e
imanwarsame <iwarsame38@gmail.com>
2023-06-18 11:51:53 +0100

    Filled in the about and contact pages

--- warsamestudio/app/about/page.tsx
+++ warsamestudio/app/about/page.tsx
@@ -1,12 +1,21 @@
 'use client';
-import { Grid, Typography } from '@mui/material';
+import { Grid, Stack, Typography } from '@mui/material';
+import ProfileImage from '../../public/Profile.jpg';
 
 export default function About() {
 
 	return (
-		<Grid container spacing={0} sx={{ width: '75%', mb: 5, mt: 1 }}>
-			<Grid item xs={12} sx={{ backgroundColor: 'transparent' }}>
-				<Typography>Ahoy</Typography>
+		<Grid container spacing={2} sx={{ width: '75%', mb: 5, mt: 10 }}>
+			<Grid item md={6} sm={12}>
+				<img src={ProfileImage.src}/>
+			</Grid>
+			<Grid item md={6} sm={12}>
+				<Stack direction='column'>
+					<Typography variant='h6'>Connecting people to great design…</Typography>
+					<Typography variant='body2' marginTop={1}>As an architectural photographer, I connect people to great design through captivating imagery. With a keen eye for detail, thoughtful use of light, and composition, I tell compelling stories that showcase exceptional designs and products.</Typography>
+					<Typography variant='body2' marginTop={1}>With a background in civil engineering, I bring a wealth of knowledge and experience in the built environment to the table. Collaborating closely with architects, developers, product manufacturers, and interior designers, I take pride in bringing your creative visions to life.</Typography>
+					<Typography variant='body2' marginTop={1}>Based primarily in London and the South-East of England, I am available for commissions both within the UK and internationally. Get in touch to discuss your next project.</Typography>
+				</Stack>
 			</Grid>
 		</Grid>
 	);
--- warsamestudio/app/contact/page.tsx
+++ warsamestudio/app/contact/page.tsx
@@ -4,10 +4,25 @@ import { Grid, Typography } from '@mui/material';
 export default function Contact() {
 
 	return (
-		<Grid container spacing={0} sx={{ width: '75%', mb: 5, mt: 1 }}>
-			<Grid item xs={12} sx={{ backgroundColor: 'transparent' }}>
-				<Typography>Contact me here</Typography>
-			</Grid>
+		<Grid container spacing={0} sx={{ width: '75%', mb: 5, mt: 10 }}>
+		<Grid item xs={12} display='flex'>
+		  <iframe
+			id="sprout-iframe"
+			src="//api.sproutstudio.com/leads/viewform/faa9942b3db2cef85f89339d995e8674d702088b3ca20ab1/453763"
+			scrolling="no"
+			style={{
+			  display: 'block',
+			  margin: '0px',
+			  border: 0,
+			  padding: '0px',
+			  width: '100%',
+			  minWidth: '100%',
+			  height: '1000px',
+			  overflow: 'hidden',
+			  backgroundColor: 'transparent'
+			}}
+		  ></iframe>
 		</Grid>
+	  </Grid>
 	);
 }
